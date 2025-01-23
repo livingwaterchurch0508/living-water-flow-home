2d989be7391271d724bfe76826f19ca3199ce6cd
bwkim <bongborobong@gimbong-uui-MacBookPro.local>
2025-01-23 22:46:11 +0900

    🔨turso -> mysql change

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,25 +6,25 @@ dotenv.config({
 });
 
 /** mysql */
-// export default {
-//   schema: "./util/db/schema.ts", // Path to your schema
-//   driver: "mysql2",
-//   dbCredentials: {
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     host: process.env.DATABASE_HOST || "",
-//     port: Number(process.env.DATABASE_PORT),
-//     database: process.env.DATABASE_SCHEMA || "",
-//   },
-// } satisfies Config;
-
-/** sqlite */
 export default {
-  out: "./drizzle",
-  schema: "./app/(util)/db/sqlite/schema.ts",
-  dialect: "turso",
+  dialect: "mysql",
+  schema: "./app/(util)/db/mysql/schema.ts", // Path to your schema
   dbCredentials: {
-    url: process.env.SQLITE_DATABASE_URL!,
-    authToken: process.env.SQLITE_DATABASE_AUTH_TOKEN,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST || "",
+    port: Number(process.env.DATABASE_PORT),
+    database: process.env.DATABASE_SCHEMA || "",
   },
 } satisfies Config;
+
+/** sqlite */
+// export default {
+//   out: "./drizzle",
+//   schema: "./app/(util)/db/sqlite/schema.ts",
+//   dialect: "turso",
+//   dbCredentials: {
+//     url: process.env.SQLITE_DATABASE_URL!,
+//     authToken: process.env.SQLITE_DATABASE_AUTH_TOKEN,
+//   },
+// } satisfies Config;
