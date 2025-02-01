2c4b06d9ae3b17e365fd415e6b39899971f2265c
bwkim <bongborobong@gimbong-uui-MacBookPro.local>
2025-02-01 23:49:18 +0900

    🔨sharp 이미지 압축

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,10 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import NodeCache from "node-cache";
+import sharp from "sharp";
 import { bucket } from "@/app/(util)/fetch/storage";
 
 // Create a new cache instance (TTL: 3600 seconds, 1 hour)
 const cache = new NodeCache({ stdTTL: 3600 });
+
+async function compressImage(buffer: Buffer): Promise<Buffer> {
+  return sharp(buffer).rotate().webp({ quality: 80 }).toBuffer(); // quality를 0~100 사이로 조절 가능
+}
 
 // Helper function to convert stream to buffer
 async function streamToBuffer(stream: NodeJS.ReadableStream): Promise<Buffer> {
@@ -48,11 +53,12 @@ export async function GET(req: NextRequest) {
   // Download the file and convert it to a buffer
   const fileStream = file.createReadStream();
   const buffer = await streamToBuffer(fileStream);
+  const compressedBuffer = await compressImage(buffer);
 
   // Cache the image buffer
-  cache.set(imageName, buffer);
+  cache.set(imageName, compressedBuffer);
 
-  return new NextResponse(buffer, {
+  return new NextResponse(compressedBuffer, {
     headers: {
       "Content-Type": "image/jpeg", // Adjust the content type as needed
       "Cache-Control": "public, max-age=3600",
